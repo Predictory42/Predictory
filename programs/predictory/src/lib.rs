f050c7a684ee0068b127a24d623f1d841e61a1a0
hlgltvnnk <olalytvinenko@gmail.com>
2025-04-29 17:01:18 +0300

    project skeleton added

---
+++ programs/predictory/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("CEYFY2CbDJ3TywKT3tzef6BcqE4NoviFTjhpURTK714U");
+
+#[program]
+pub mod predictory {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
